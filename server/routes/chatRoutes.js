8617cb0c3c1836646837d2d225d3843a755bd827
arnV17 <arnavog@gmail.com>
2025-08-24 13:25:28 +0530

    newnwe

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 const model = new ChatGoogleGenerativeAI({
   model: process.env.GOOGLE_MODEL || "gemini-1.5-flash",
-  apiKey: process.env.GOOGLE_API_KEY,
+  apiKey: process.env.GOOGLE_API_KEY2,
 });
 
 const simpleRoutes = {
